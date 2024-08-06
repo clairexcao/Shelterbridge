675e91466c4e9d0a43cd1ffa425caa7f71eca189
Claire Cao <clairexuancao@gmail.com>
2024-08-06 12:46:41 -0700

    try to fix the post review screen problem

--- frontend/screens/ResourceDetails.js
+++ frontend/screens/ResourceDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView, StyleSheet, Linking } from 'react-native';
+import { KeyboardAvoidingView, View, Text, ScrollView, StyleSheet, Linking } from 'react-native';
 import axios from 'axios';
 import ReviewComponent from './ReviewComponent';
 import styles from '../styles/ResourceDetailsStyles';
@@ -49,55 +49,57 @@ const ResourceDetails = ({ route }) => {
     };
 
     return (
-        <ScrollView style={styles.container}>
-            <Text style={styles.title}>{resource.name}</Text>
-            <Text style={styles.description}>{resource.description}</Text>
-            <View style={styles.infoBox}>
-                <Text style={styles.label}>Address</Text>
-                <Text style={styles.content}>{resource.address}</Text>
-            </View>
-            {resource.eligibility ? (<View style={styles.infoBox}>
-                <Text style={styles.label}>Eligibility</Text>
-                <Text style={styles.content}>{resource.eligibility}</Text>
-            </View>) : null
-            }
-            {resource.hours ? (< View style={styles.infoBox}>
-                <Text style={styles.label}>Hours</Text>
-                <Text style={styles.content}>{resource.hours}</Text>
-            </View>) : null
-            }
-            {resource.email ? (<View style={styles.infoBox}>
-                <Text style={styles.label}>Email</Text>
-                <Text style={styles.content}>{resource.email}</Text>
-            </View>) : null
-            }
-            {resource.phone ? (<View style={styles.infoBox}>
-                <Text style={styles.label}>Phone Number</Text>
-                <Text style={styles.content}>{resource.phone}</Text>
-            </View>) : null
-            }
-            {resource.directions ? (<View style={styles.infoBox}>
-                <Text style={styles.label}>Directions</Text>
-                <Text style={styles.content}>{resource.directions}</Text>
-            </View>) : null
-            }
-            <View style={styles.infoBox}>
-                <Text style={styles.label}>Website</Text>
-                <Text style={styles.contentLink} onPress={() => openURL(resource.website)}>{resource.website}</Text>
-            </View>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <ScrollView style={styles.container}>
+                <Text style={styles.title}>{resource.name}</Text>
+                <Text style={styles.description}>{resource.description}</Text>
+                <View style={styles.infoBox}>
+                    <Text style={styles.label}>Address</Text>
+                    <Text style={styles.content}>{resource.address}</Text>
+                </View>
+                {resource.eligibility ? (<View style={styles.infoBox}>
+                    <Text style={styles.label}>Eligibility</Text>
+                    <Text style={styles.content}>{resource.eligibility}</Text>
+                </View>) : null
+                }
+                {resource.hours ? (< View style={styles.infoBox}>
+                    <Text style={styles.label}>Hours</Text>
+                    <Text style={styles.content}>{resource.hours}</Text>
+                </View>) : null
+                }
+                {resource.email ? (<View style={styles.infoBox}>
+                    <Text style={styles.label}>Email</Text>
+                    <Text style={styles.content}>{resource.email}</Text>
+                </View>) : null
+                }
+                {resource.phone ? (<View style={styles.infoBox}>
+                    <Text style={styles.label}>Phone Number</Text>
+                    <Text style={styles.content}>{resource.phone}</Text>
+                </View>) : null
+                }
+                {resource.directions ? (<View style={styles.infoBox}>
+                    <Text style={styles.label}>Directions</Text>
+                    <Text style={styles.content}>{resource.directions}</Text>
+                </View>) : null
+                }
+                <View style={styles.infoBox}>
+                    <Text style={styles.label}>Website</Text>
+                    <Text style={styles.contentLink} onPress={() => openURL(resource.website)}>{resource.website}</Text>
+                </View>
 
-            <ReviewComponent id={resource.id} onSubmit={handleReviewSubmit} />
-            {
-                reviews.map((review, index) => (
-                    <View key={index} style={styles.reviewContainer}>
-                        <Text style={styles.reviewName}>{review.name}</Text>
-                        <Text style={styles.reviewRating}>{review.rating} Stars</Text>
-                        <Text style={styles.reviewDate}>{review.date}</Text>
-                        <Text style={styles.reviewText}>{review.review}</Text>
-                    </View>
-                ))
-            }
-        </ScrollView >
+                <ReviewComponent id={resource.id} onSubmit={handleReviewSubmit} />
+                {
+                    reviews.map((review, index) => (
+                        <View key={index} style={styles.reviewContainer}>
+                            <Text style={styles.reviewName}>{review.name}</Text>
+                            <Text style={styles.reviewRating}>{review.rating} Stars</Text>
+                            <Text style={styles.reviewDate}>{review.date}</Text>
+                            <Text style={styles.reviewText}>{review.review}</Text>
+                        </View>
+                    ))
+                }
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
 
